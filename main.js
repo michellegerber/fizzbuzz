8b765ea6b6e7d250658d5b7522382eb41caa0b24
michellegerber <michellegerber03@gmail.com>
2015-10-09 00:19:43 +0000

    fizzbuzz loop and alert

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-   $("button").on("click", getInput);
+   $("button").on("click", getData);
 });
+function getNumber (input) {
+for( var i=1; i<=input; i++) {
+        if ((i%5===0) && (i%3===0))
+          { $(".num").append( "FizzBuzz" + "<br>");}
+        else if (i%3===0)
+          { $(".num").append( "Fizz" + "<br>");}
+        else if  (i%5===0) 
+          { $(".num").append(  "Buzz" + "<br>");}
+        else { $(".num").append( i + "<br>");}  
+		}
+}
+ function getData() {
+    var custInput = $(".datainput");
+    var input = custInput.val();
+    
+         if ((input !== "") && (input <= 100) && (input >= 1) && ($.trim(input) !== "")) 
+{
+      
+    $(".num").html('');
+   getNumber(input);
+    custInput.val("");
+
+  }
+  else alert("Whoops! Try a number out!");
+  }
