044bb9a1d6a065ffb11c3fec899137eb800da406
michellegerber <michellegerber03@gmail.com>
2015-10-09 00:12:05 +0000

    deleting second line helper

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
-  
-for (var number = 0; number <= 101; number = number + 2)
-  console.log(number);
+$(document).ready(function() {
+   $("button").on("click", getInput);
+});
